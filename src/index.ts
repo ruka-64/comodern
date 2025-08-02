385dcab2942c8459d8e0db6038c190a37a8f6bb4
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-08-03 00:14:30 +0900

    (v0.1.2) performance fix

--- src/index.ts
+++ src/index.ts
@@ -12,68 +12,59 @@ const logLevels: LogLevelTypes[] = [
   { level: 3, label: 'error' },
 ];
 
-export class Logger {
-  private level: number;
-  constructor(level: string) {
-    this.level =
-      logLevels.find((l) => l.label === level)?.level ?? logLevels[0].level;
-  }
+export function Logger(level: string) {
+  const actualLevel =
+    logLevels.find((l) => l.label === level)?.level ?? logLevels[0].level;
 
-  log(str: any, ...args: any[]) {
-    this.printLogs(0, str, args);
-  }
-  info(str: any, ...args: any[]) {
-    this.printLogs(1, str, args);
-  }
-  warn(str: any, ...args: any[]) {
-    this.printLogs(2, str, args);
-  }
-  error(str: any, ...args: any[]) {
-    this.printLogs(3, str, args);
-  }
-  success(str: any, ...args: any[]) {
-    if (1 >= this.level) {
-      console.log(`${color.greenBright(` ✔ `)} ${str}`, ...args);
-    }
-  }
-  fail(str: any, ...args: any[]) {
-    if (1 >= this.level) {
-      console.log(`${color.redBright(` ✗ `)} ${str}`, ...args);
+  function defPrefix(level: number): string {
+    switch (level) {
+      case 0:
+        return color.bgBlackBright(color.white(` > `));
+      case 1:
+        return color.bgCyan(color.black(` i `));
+      case 2:
+        return color.bgYellow(color.black(` ! `));
+      case 3:
+        return color.bgRed(color.black(` × `));
+      default:
+        throw new Error(`Unknown level detected (level: ${level})`);
     }
   }
 
-  private defPrefix(level: number) {
-    if (level === 0) {
-      return color.bgBlackBright(color.white(` > `));
-    }
-    if (level === 1) {
-      return color.bgCyan(color.black(` i `));
-    }
-    if (level === 2) {
-      return color.bgYellow(color.black(` ! `));
-    }
-    if (level === 3) {
-      return color.bgRed(color.black(` × `));
-    } else {
-      throw new Error(`Unknown level detected (level: ${level})`);
-    }
-  }
+  const printLogs = (level: number, str: any, args: any[]) => {
+    if (level >= actualLevel) {
+      const prefix = defPrefix(level);
 
-  private printLogs(level: number, str: any, args: any[]) {
-    if (level >= this.level) {
-      const p = this.defPrefix(level);
-      //Object detection
+      // オブジェクトの検出
       if (Object.prototype.toString.call(str) === '[object Object]') {
-        return console.log(p + color.gray(' Object:\n'), str, ...args);
+        return console.log(prefix + color.gray(' Object:\n'), str, ...args);
       }
-      //Error message detection
+
+      // エラーメッセージの検出
       if (str.stack) {
-        return console.error(p, str, ...args);
+        return console.error(prefix, str, ...args);
       } else {
-        return console.log(p, str, ...args);
+        return console.log(prefix, str, ...args);
       }
     }
-  }
+  };
+
+  return {
+    log: (str: any, ...args: any[]) => printLogs(0, str, args),
+    info: (str: any, ...args: any[]) => printLogs(1, str, args),
+    warn: (str: any, ...args: any[]) => printLogs(2, str, args),
+    error: (str: any, ...args: any[]) => printLogs(3, str, args),
+    success: (str: any, ...args: any[]) => {
+      if (1 >= actualLevel) {
+        console.log(`${color.greenBright(` ✔ `)} ${str}`, ...args);
+      }
+    },
+    fail: (str: any, ...args: any[]) => {
+      if (1 >= actualLevel) {
+        console.log(`${color.redBright(` ✗ `)} ${str}`, ...args);
+      }
+    },
+  };
 }
 
-export const logger = new Logger('debug');
+export const logger = Logger('debug');
